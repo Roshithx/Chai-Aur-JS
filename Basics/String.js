22f41e3585217919f0fd696b36b817d1a93c2d65
Roshith P <125194847+Roshithx@users.noreply.github.com>
2024-09-26 11:24:04 +0530

    strings

---
+++ Basics/String.js
@@ -0,0 +1,30 @@
+const name="Roshi"
+// console.log(name[0]);
+// console.log(name.__proto__);
+
+// console.log(name.toUpperCase());
+// console.log(name);
+
+// console.log(name.charAt(2));
+// console.log(name.indexOf('R'));
+
+// console.log(name.substring(0 ,2));
+// console.log(name.slice(-5,4));
+
+ let names = "    bublu kunji "
+ console.log(names);
+ console.log(names.trim());
+
+  console.log(names.split(""));
+  
+ 
+ 
+ 
+
+
+
+
+
+
+
+
