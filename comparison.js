96e708533c3179e91545c8479c1290d5713f298f
Roshith P <125194847+Roshithx@users.noreply.github.com>
2024-09-26 09:53:33 +0530

    comparison

---
+++ comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>=1);
+
+
+// console.log("2">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null<=0); // comparison operator <= // >= convert null to 0
+
+// console.log(undefined==0);
+// console.log(undefined<=0);
+// console.log(undefined>=0);
+
+// ===
+
+console.log("2"==2);
+
+
+
+
+
+
--- datatypeConversion.js
+++ datatypeConversion.js
@@ -1,26 +1,63 @@
-let score="3A3"
-console.log(typeof score);
+// let score="3A3"
+// console.log(typeof score);
 
-let valueInNumber= Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber); 
-
-
-//"33" => 33 
-// "3A3"=> NaN but type is number after conversion
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
 
 
-let isLoggedIn=""
-let booleanCheck = Boolean(isLoggedIn)
-console.log(booleanCheck);
+// //"33" => 33 
+// // "3A3"=> NaN but type is number after conversion
 
-//1 => true ; 0 => false
-// "" => false ; "Ros" => true
 
-let someNum=33
-let stringnum=String(someNum)
-console.log(stringnum);
-console.log(typeof stringnum);
+// let isLoggedIn=""
+// let booleanCheck = Boolean(isLoggedIn)
+// console.log(booleanCheck);
+
+// //1 => true ; 0 => false
+// // "" => false ; "Ros" => true
+
+// let someNum=33
+// let stringnum=String(someNum)
+// console.log(stringnum);
+// console.log(typeof stringnum);
+
+
+// ************************************OPERATIONS********************************//
+
+//  let value=3
+//  let negValue = -value
+//  console.log(negValue);
+
+//  let str1="Hello"
+//  let str2="Roshi"
+//  let str3=str1+str2
+//  console.log(str3);
+
+//  console.log("1"+2);
+//  console.log(1+"2");
+//  console.log(1+2+"3");
+//  console.log("3"+1+2);
+ 
+
+console.log(+true);
+console.log(+"");
+
+let Counter=100
+++Counter
+
+console.log(Counter);
+
+
+
+
+ 
+
+ 
+ 
+ 
+ 
+ 
 
 
 
