576beca3194f097d361e38c6b9f0d429e06bfd09
Roshith P <125194847+Roshithx@users.noreply.github.com>
2024-09-26 12:04:45 +0530

    Math&Num

---
+++ Basics/Num_Maths.js
@@ -0,0 +1,40 @@
+// const score = 400
+// console.log(score);
+
+// const balance=new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNo=23.56565
+// console.log(otherNo.toPrecision(3));
+
+// const hundred=1000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+//********************************MATHS*****************************************/
+
+//   console.log(Math);
+//   console.log(Math.abs(-5)); // change -ve to +ve
+//   console.log(Math.round(4.6)); // round to 
+//   console.log(Math.ceil(4.5));
+//   console.log(Math.floor(4.5));
+//   console.log(Math.random());
+//   console.log(Math.random()*10+1);
+
+  const min=10
+  const max=20
+  console.log(Math.floor(Math.random()*(max-min+1)) + min)
+  
+  
+
+  
+  
+  
+  
+  
+    
+
+
