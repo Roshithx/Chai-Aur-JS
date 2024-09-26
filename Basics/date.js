db0af7a8ac085f0e1be3a0c553493946bf774999
Roshith P <125194847+Roshithx@users.noreply.github.com>
2024-09-26 12:32:36 +0530

    date

---
+++ Basics/date.js
@@ -0,0 +1,13 @@
+let myDate=new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toJSON());
+
+
+
+  
+let myNewDate=new Date(2024,2,23)
+console.log(myNewDate.toDateString());
+
+let timestamp= Date.now()
+console.log(Math.floor(Date.now()/1000));
